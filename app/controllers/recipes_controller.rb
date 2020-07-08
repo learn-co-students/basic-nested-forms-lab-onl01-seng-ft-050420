e15bcc370dd98dcd0f85281d72f6ba6a44ca5c05
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-07-08 00:35:56 -0400

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,24 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build(quantity: "1")
+    @recipe.ingredients.build(quantity: "1")
   end
 
   def create
+   #"recipe"=>{"title"=>"Salad", "ingredients_attributes"=>{"0"=>{"name"=>"Lettuce", "quantity"=>"1 head"}}},
+    recipe = Recipe.create(recipe_params)
+    redirect_to recipe_path(recipe)
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(
+      :title,
+      ingredients_attributes: [
+        :name,
+        :quantity
+      ])
   end
 end
