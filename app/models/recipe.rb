5b6378367d17b36e00f5915da0ce1e2dfedf2685
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-07-10 11:10:26 -0400

    Done.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,13 @@
 class Recipe < ActiveRecord::Base
-  has_many :ingredients 
+  has_many :ingredients
+  #accepts_nested_attributes_for :ingredients, allow_destroy: true
+
+
+  def ingredients_attributes=(ingredients_attributes)
+
+    ingredients_attributes.each do |i, ingredients_attribute|
+      self.ingredients.build(ingredients_attribute)
+    end
+  end
+
 end
