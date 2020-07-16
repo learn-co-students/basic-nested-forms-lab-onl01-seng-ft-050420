b98168846e5853ce6eb0f7d6dc445d85907924cb
anette87 <anettem.rivera@gmail.com>
2020-07-16 10:21:33 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+recipe1 = Recipe.create(title: "Birthday Cake")
+recipe2 = Recipe.create(title: "Oatmeal Cookies")
+
+ingredient1 = Ingredient.create(name: "Eggs", quantity: "2", recipe_id: 1)
+ingredient2 = Ingredient.create(name: "Flour", quantity: "2 cups", recipe_id: 1)
+ingredient3 = Ingredient.create(name: "Honey", quantity: "1/2 cup", recipe_id: 2)
+ingredient4 = Ingredient.create(name: "Oats", quantity: "1 cup", recipe_id: 2)
+
+
